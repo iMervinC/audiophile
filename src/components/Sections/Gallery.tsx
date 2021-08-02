992ca7e6ff8ab3550a2d0830cb7821172b62d020
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-08-02 08:05:36 +0800

    fix

--- src/components/Sections/Gallery.tsx
+++ src/components/Sections/Gallery.tsx
@@ -6,36 +6,36 @@ export const Gallery = ({ gallery }: { gallery: Products['gallery'] }) => {
     <div className="mt-16 contain-padding grid grid-cols-1 gap-[30px] md:grid-cols-gallery-tablet md:grid-rows-gallery-tablet lg:grid-cols-gallery lg:grid-rows-gallery">
       <div className="sm:col-start-1">
         <Image
-          src={gallery.first.desktop}
+          src={gallery!.first.desktop}
           alt="first"
           height="280"
           width="445"
           objectFit="fill"
-          blurDataURL={gallery.first.mobile}
+          blurDataURL={gallery!.first.mobile}
           placeholder="blur"
           className="rounded-xl"
         />
       </div>
       <div className="sm:col-start-1 sm:row-start-2">
         <Image
-          src={gallery.second.desktop}
+          src={gallery!.second.desktop}
           alt="second"
           height="280"
           width="445"
           objectFit="cover"
-          blurDataURL={gallery.second.mobile}
+          blurDataURL={gallery!.second.mobile}
           placeholder="blur"
           className="rounded-xl"
         />
       </div>
       <div className="sm:col-start-2 sm:row-start-1 sm:row-end-2">
         <Image
-          src={gallery.third.desktop}
+          src={gallery!.third.desktop}
           alt="third"
           height="592"
           width="635"
           objectFit="cover"
-          blurDataURL={gallery.third.mobile}
+          blurDataURL={gallery!.third.mobile}
           placeholder="blur"
           className="rounded-xl"
         />
